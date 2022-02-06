032a3b93f84a98e01dddc00f3887a9256c3a6a53
kanishkkavdia <61052487+kanishkkavdia@users.noreply.github.com>
2022-02-06 23:09:52 +0530

    Add files via upload

---
+++ pdf_extractor.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+from urllib.parse import urlsplit
+import json
+# My custom python codes
+# from link_extractor import LinkExtractor
+
+
+pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files(x86)\Tesseract-OCR\tesseract.exe"
+
+Url=pd.read_csv("data_pdf.csv")["Url"]
+
+webbased_url=[]
+pdfbased_url=[]
+
+
+for x in Url:
+    if ".pdf" in x:
+        try:
+            text_obj=Pdf2processor(x)
+            pdfbased_url.append({"page-url":str(x),"pdf-url":str(x),"pdf-content":text_obj.generate_text()})
+        except:
+            continue
+    else:
+        try:
+            obj=LinkExtractor(x)
+            text_obj=Pdf2processor(obj.get_pdf_link())
+            webbased_url.append({"page-url":str(x),"pdf-url":str(obj.get_pdf_link()),"pdf-content":text_obj.generate_text()})
+        except:
+            continue
+        
+
+json_object=webbased_url+pdfbased_url
+json_object=json.dumps(json_object,indent=3)
+print(json_object)
+
+with open("pdf_data.json", "w") as outfile:
+    outfile.write(json_object)
